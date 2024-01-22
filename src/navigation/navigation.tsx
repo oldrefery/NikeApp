e443823b43940d321f59d5d9f9c98e9fbcf4b817
Rustem.Shakirov <Rustem.Shakirov@emea.merkleinc.com>
2024-01-22 22:48:05 +0100

    style: add shop cart navigation in header and add style to navigation

--- src/navigation/navigation.tsx
+++ src/navigation/navigation.tsx
@@ -3,15 +3,47 @@ import { NavigationContainer } from "@react-navigation/native";
 import { ProductScreen } from "../screens/ProductScreen";
 import { ProductDetailsScreen } from "../screens/ProductDetailsScreen";
 import { ShoppingCartScreen } from "../screens/ShoppingCartScreen";
+import { Pressable, StyleSheet, Text } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
 import { RootStackParamList } from "./types";
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
+const styles = StyleSheet.create({
+  cartContainer: {
+    flexDirection: "row",
+  },
+  cartText: {
+    marginLeft: 5,
+    fontWeight: "500",
+  },
+  commonStyle: {
+    backgroundColor: "white",
+  },
+});
 export const Navigation = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Products" component={ProductScreen} />
+      <Stack.Navigator
+        screenOptions={{
+          contentStyle: styles.commonStyle,
+        }}
+      >
+        <Stack.Screen
+          name="Products"
+          component={ProductScreen}
+          options={({ navigation }) => ({
+            headerRight: () => (
+              <Pressable
+                onPress={() => navigation.navigate("Cart")}
+                style={styles.cartContainer}
+              >
+                <FontAwesome name="shopping-cart" size={18} color="gray" />
+                <Text style={styles.cartText}>1</Text>
+              </Pressable>
+            ),
+          })}
+        />
         <Stack.Screen name="Product Details" component={ProductDetailsScreen} />
         <Stack.Screen name="Cart" component={ShoppingCartScreen} />
       </Stack.Navigator>
